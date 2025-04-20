f6c53cafa01f407f1888c9362974b40e82d6f802
Sylvain GENEAU de LAMARLIERE <mirouf18@gmail.com>
2025-04-21 01:18:20 +0200

    Add home page with featured article component (#8)
    * Home page and feature article
    
    * Fix background image path not loading on gh pages
    
    * fix image layout for feature article

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
-import { NewsListComponent }        from './features/news-list/news-list.component';
-import { ContactComponent }         from './features/contact/contact.component';
-import { NewsDetailComponent }      from './features/news-detail/news-detail.component';
+import { NewsListComponent } from './features/news-list/news-list.component';
+import { ContactComponent } from './features/contact/contact.component';
+import { NewsDetailComponent } from './features/news-detail/news-detail.component';
+import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'news-detail/:id', component: NewsDetailComponent },
-  { path: 'news',         component: NewsListComponent },
-  { path: 'contact',      component: ContactComponent },
-  { path: '**',           redirectTo: '' }
+  { path: 'news', component: NewsListComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '' },
 ];
